07c15953c14b9ba4b989b5eec107195a972be140
Ilya Shadrin <90275369+dekey111@users.noreply.github.com>
2026-06-02 20:17:44 +0300

    Этап 8. Создание схемы базы данных

--- YAP_middle-csharp/YAP_middle-csharp/Program.cs
+++ YAP_middle-csharp/YAP_middle-csharp/Program.cs
@@ -56,6 +56,11 @@ builder.Services.AddProblemDetails(options =>
 });
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureCreated(); 
+}
 app.UseMiddleware<GlobalExceptionHandlingMiddleware>();
 
 // Configure the HTTP request pipeline.
