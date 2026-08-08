8d9c2d366c746aa1092c13357034db509ef44412
Ilya Shadrin <90275369+dekey111@users.noreply.github.com>
2026-08-08 08:30:35 +0300

    Этап 3. Разделяемый контракт события

---
+++ YAP_middle_csharp.Contracts/YAP_middle_csharp.Contracts/BookingModel/BookingConfirmedEvent.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YAP_middle_csharp.Contracts.BookingModel
+{
+    public record BookingConfirmedEvent( Guid BookingId, Guid EventId, Guid UserId, int SeatsCount, DateTime ConfirmedAt );
+}
