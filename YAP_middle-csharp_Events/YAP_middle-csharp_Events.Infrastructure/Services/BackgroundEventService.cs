473e7baf494fbfdeadf955cea15e2d36a6c7dece
Ilya Shadrin <90275369+dekey111@users.noreply.github.com>
2026-08-23 01:19:29 +0300

    Fix2. CancellationToken проброшен, но в трёх местах всё ещё отсутствует

--- YAP_middle-csharp_Events/YAP_middle-csharp_Events.Infrastructure/Services/BackgroundEventService.cs
+++ YAP_middle-csharp_Events/YAP_middle-csharp_Events.Infrastructure/Services/BackgroundEventService.cs
@@ -130,7 +130,7 @@ namespace YAP_middle_csharp_Events.Infrastructure.Services
                 try
                 {
                     eventModel.AvailableSeats -= bookingConfirmedEvent.SeatsCount;
-                    await eventRepository.UpdateAsync(eventModel);
+                    await eventRepository.UpdateAsync(eventModel, cancellationToken);
 
                     await processedRepository.AddAsync(new ProcessedBookingsModel(bookingConfirmedEvent.BookingId), cancellationToken);
 
